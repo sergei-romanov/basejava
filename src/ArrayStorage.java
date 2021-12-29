605708e01ede7cbe49b5eeb1d31ed1edb9105c7f
Sergey Romanov <romanov.java@gmail.com>
2021-12-29 20:53:05 +0300

    HW1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,47 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+
+    private final Resume[] storage = new Resume[10000];
+    private int position = 0;
 
     void clear() {
+        position = 0;
     }
 
     void save(Resume r) {
+        storage[position++] = r;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < position; i++) {
+            if (uuid.equalsIgnoreCase(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < position; i++) {
+            if (uuid.equalsIgnoreCase(storage[i].uuid)) {
+                System.arraycopy(storage, i + 1, storage, i, position - i - 1);
+                position--;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, position);
     }
 
     int size() {
-        return 0;
+        return position;
     }
 }
