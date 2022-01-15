254d07bec445a8dcc52a55f45772214c704b7d41
Sergey Romanov <romanov.java@gmail.com>
2022-01-15 13:31:30 +0300

    Lesson07_HW06 getAllSorted

--- src/com/urise/webapp/storage/MapUuidStorage.java
+++ src/com/urise/webapp/storage/MapUuidStorage.java
@@ -2,7 +2,9 @@ package com.urise.webapp.storage;
 
 import com.urise.webapp.model.Resume;
 
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MapUuidStorage extends AbstractStorage {
@@ -43,8 +45,8 @@ public class MapUuidStorage extends AbstractStorage {
     }
 
     @Override
-    public Resume[] getAll() {
-        return new Resume[0];
+    public List<Resume> doCopyAll() {
+        return Collections.emptyList();
     }
 
     @Override
