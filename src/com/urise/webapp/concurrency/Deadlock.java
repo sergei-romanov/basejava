31a4b8516f9cb125152d694a52c1bcfb15415da3
Sergey Romanov <romanov.java@gmail.com>
2022-02-15 14:02:16 +0300

    HW11 update Deadlock

--- src/com/urise/webapp/concurrency/Deadlock.java
+++ src/com/urise/webapp/concurrency/Deadlock.java
@@ -3,43 +3,46 @@ package com.urise.webapp.concurrency;
 public class Deadlock {
     public static final Object lock1 = new Object();
     public static final Object lock2 = new Object();
+
     public static void main(String[] args) {
-        Thread10 thread10 = new Thread10();
-        Thread20 thread20 = new Thread20();
-        thread10.start();
-        thread20.start();
+        Thread1 thread1 = new Thread1();
+        Thread2 thread2 = new Thread2();
+        thread1.start();
+        thread2.start();
     }
 }
-class Thread10 extends Thread {
+
+class Thread1 extends Thread {
     public void run() {
         synchronized (Deadlock.lock1) {
-            System.out.println("THread10: Попытка захватить монитор объекта lock1");
-            System.out.println("THread10: монитор объекта lock1 захвачен");
-            System.out.println("THread10: Попытка захватить монитор объекта lock2");
-         /*   try {
+            System.out.println("Thread1: Попытка захватить монитор объекта lock1");
+            System.out.println("Thread1: монитор объекта lock1 захвачен");
+            System.out.println("Thread1: Попытка захватить монитор объекта lock2");
+            try {
                 sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }*/
+            }
             synchronized (Deadlock.lock2) {
-                System.out.println("THread10: монитор объекта lock1 и lock2 захвачены");
+                System.out.println("Thread1: монитор объекта lock1 и lock2 захвачены");
             }
         }
     }
 }
-class Thread20 extends Thread {
+
+class Thread2 extends Thread {
     public void run() {
         synchronized (Deadlock.lock2) {
-            System.out.println("THread20: Попытка захватить монитор объекта lock2");
-            System.out.println("THread20: монитор объекта lock2 захвачен");
-            System.out.println("THread20: Попытка захватить монитор объекта lock1");
-           /* try {
+            System.out.println("Thread2: Попытка захватить монитор объекта lock2");
+            System.out.println("Thread2: монитор объекта lock2 захвачен");
+            System.out.println("Thread2: Попытка захватить монитор объекта lock1");
+            try {
                 sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            }*/
+            }
             synchronized (Deadlock.lock1) {
-                System.out.println("THread20: монитор объекта lock1 и lock2 захвачены");
+                System.out.println("Thread2: монитор объекта lock1 и lock2 захвачены");
             }
         }
     }
