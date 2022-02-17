e0342c3dbc4f0e7360efb9938b394041285c3063
Sergey Romanov <romanov.java@gmail.com>
2022-02-17 16:27:39 +0300

    Lesson12 Concurrency update

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -17,12 +17,10 @@ public class MainConcurrency {
     private static final ReentrantReadWriteLock reentrantReadWriteLock = new ReentrantReadWriteLock();
     private static final Lock WRITE_LOCK = reentrantReadWriteLock.writeLock();
     private static final Lock READ_LOCK = reentrantReadWriteLock.readLock();
-    private static final ThreadLocal<SimpleDateFormat> threadLocal = new ThreadLocal<SimpleDateFormat>() {
-        @Override
-        protected SimpleDateFormat initialValue() {
-            return new SimpleDateFormat();
-        }
-    };
+    private static final ThreadLocal<SimpleDateFormat> threadLocal = ThreadLocal.withInitial(() ->
+            new SimpleDateFormat("dd.MM.yyyy HH:mm:ss"));
+
+
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println(Thread.currentThread().getName());
