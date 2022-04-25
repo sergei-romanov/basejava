7f8532dc0cfb343be756cb5dda5aba34360452bd
Sergey Romanov <romanov.java@gmail.com>
2022-04-25 19:59:42 +0300

    Lesson16 HW15_DB

--- config/initDB.sql
+++ config/initDB.sql
@@ -2,22 +2,28 @@ drop table IF EXISTS section;
 drop table IF EXISTS contact;
 drop table IF EXISTS resume;
 
-create table resume
+CREATE TABLE resume
 (
-    uuid      char(36) not null constraint resume_pk primary key,
-    full_name text not null
+    uuid      CHAR(36) PRIMARY KEY NOT NULL,
+    full_name TEXT                 NOT NULL
 );
 
-alter table resume owner to postgres;
-
-create table contact
+CREATE TABLE contact
 (
-    id          serial constraint contact_pk primary key,
-    type        text     not null,
-    value       text     not null,
-    resume_uuid char(36) not null constraint contact_resume_uuid_fk references resume on update restrict on delete cascade
+    id          SERIAL,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    value       TEXT     NOT NULL
 );
+CREATE UNIQUE INDEX contact_uuid_type_index
+    ON contact (resume_uuid, type);
 
-alter table contact owner to postgres;
-
-create unique index contact_uuid_type_index on contact (resume_uuid, type);
+CREATE TABLE section
+(
+    id          SERIAL PRIMARY KEY,
+    resume_uuid CHAR(36) NOT NULL REFERENCES resume (uuid) ON DELETE CASCADE,
+    type        TEXT     NOT NULL,
+    content     TEXT     NOT NULL
+);
+CREATE UNIQUE INDEX section_idx
+    ON section (resume_uuid, type);
---
+++ test/com/urise/webapp/storage/JsonParserTest.java
@@ -0,0 +1,29 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.model.Section;
+import com.urise.webapp.model.TextSection;
+import com.urise.webapp.util.JsonParser;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static com.urise.webapp.storage.TestData.R1;
+
+public class JsonParserTest {
+    @Test
+    public void testResume() throws Exception {
+        String json = JsonParser.write(R1);
+        System.out.println(json);
+        Resume resume = JsonParser.read(json, Resume.class);
+        Assert.assertEquals(R1, resume);
+    }
+
+    @Test
+    public void write() throws Exception {
+        Section section1 = new TextSection("Objective1");
+        String json = JsonParser.write(section1, Section.class);
+        System.out.println(json);
+        Section section2 = JsonParser.read(json, Section.class);
+        Assert.assertEquals(section1, section2);
+    }
+}
