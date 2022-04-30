517f164f38b1b9f815d96bdfd8862804c7024246
Sergey Romanov <romanov.java@gmail.com>
2022-04-30 18:43:30 +0300

    Lesson16 HW15_servlet

--- src/com/urise/webapp/Config.java
+++ src/com/urise/webapp/Config.java
@@ -10,7 +10,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("config\\resumes.properties");
+    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
     private static final Config INSTANCE = new Config();
 
     private final File storageDir;
@@ -30,11 +30,21 @@ public class Config {
             throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
         }
     }
+
     public File getStorageDir() {
         return storageDir;
     }
 
     public Storage getStorage() {
         return storage;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
